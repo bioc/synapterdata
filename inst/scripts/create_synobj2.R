c0113e8e9148c0b61db319deb4cbea25c977db71
Laurent Gatto <l.gatto@dnavision.be>
2017-04-04 08:23:51 +0000

    add fasta and replace synobj2
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/synapterdata@4109 db2202da-8704-0410-b924-b125b3cd84ef

--- inst/scripts/create_synobj2.R
+++ inst/scripts/create_synobj2.R
@@ -3,22 +3,13 @@
 library("synapter")
 
 inlist <- list(
+  identpeptide = "fermentor_03_sample_01_HDMSE_01_IA_final_peptide.csv.gz",
+  identfragments = "fermentor_03_sample_01_HDMSE_01_IA_final_fragment.csv.gz",
   quantpeptide = "fermentor_02_sample_01_HDMSE_01_IA_final_peptide.csv.gz",
   quantpep3d = "fermentor_02_sample_01_HDMSE_01_Pep3DAMRT.csv.gz",
-  identpeptide = "fermentor_03_sample_01_HDMSE_01_IA_final_peptide.csv.gz",
+  quantspectra = "fermentor_02_sample_01_HDMSE_01_Pep3D_Spectrum.xml.gz",
   fasta = "S.cerevisiae_Uniprot_reference_canonical_18_03_14.fasta")
 
-spectrumlist <- list(
-  identspectrum = "fermentor_03_sample_01_HDMSE_01_Pep3D_Spectrum.xml.gz",
-  quantspectrum = "fermentor_02_sample_01_HDMSE_01_Pep3D_Spectrum.xml.gz")
+synobj2 <- Synapter(inlist, master=FALSE)
 
-fragmentlist <- list(
-  identfragments = "fermentor_03_sample_01_HDMSE_01_IA_final_fragment.csv.gz",
-  quantfragments = "fermentor_02_sample_01_HDMSE_01_IA_final_fragment.csv.gz")
-
-synobj2 <- synergise(inlist, outputdir = tempdir())
-loadSpectrumXmlFiles(synobj2, spectrumlist)
-loadFragmentCsvFiles(synobj2, fragmentlist)
-
-save(synobj2, file = "synobj2.rda")
-
+save(synobj2, file="synobj2.rda", compress="xz")
